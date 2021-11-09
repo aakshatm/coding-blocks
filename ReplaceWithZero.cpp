0314f2433306ea78fed749786fc97606e74d939d
Aakshat Malhotra <aakshatmalhotra100@gmail.com>
2021-11-09 12:05:15 +0530

    added replacewithzero

---
+++ ReplaceWithZero.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int evaluateLength(int n){
+    int length = 0;
+    while (n > 0){
+        n = n/10;
+        length++;
+    }
+    return length;
+}
+
+int main()
+{
+    long long int n;
+    cin >> n;
+    int index = 0;
+    long long newNum = 0;
+    int length = evaluateLength(n);
+    while (n > 0){
+        int digit; 
+        digit = n % 10;
+        if (digit == 0){
+            digit = 5; 
+            newNum = newNum + digit*pow(10, index);
+        }
+        else{
+            newNum = newNum + digit*pow(10, index);
+        }
+        index++;
+        n = n /10;
+    }
+
+    cout << newNum;
+    return 0;
+}
